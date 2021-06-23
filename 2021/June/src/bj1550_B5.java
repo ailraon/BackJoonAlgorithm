3b002e28c1620ed84531b85caac70cd786bcacdf
ailraon <aadsf1789@naver.com>
2021-06-23 22:55:06 +0900

    bj1550_B5 문제풀이

---
+++ 2021/June/src/bj1550_B5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class bj1550_B5 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println(Integer.parseInt(s.next(), 16));
+	}
+}
